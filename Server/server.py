5f5625b7baffc75c98ad4c296932da3d947e70bb
Machete <djo77515@hotmail.fr>
2020-01-10 02:25:55 +0100

    ajout page test ajout bouton flow ajout bouton rgb
    Signed-off-by: Machete <djo77515@hotmail.fr>

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from yeelight import Bulb, RGBTransition, Flow, transitions
+from yeelight import Bulb, RGBTransition, Flow, transitions, SleepTransition
 
 bulb0 = Bulb("192.168.1.181")
 bulb1 = Bulb("192.168.1.182")
@@ -8,55 +8,43 @@ bulb1 = Bulb("192.168.1.182")
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    
+def index():    
     return (render_template('index.html'), 200)
 
+@app.route('/test_page')
+def test():
+    return render_template('test_page.html')
 
-# Turn the bulb on.
-@app.route('/start_yeelight/<int:id_bulb>')
-def start_yeelight(id_bulb=None):
-    if id_bulb != None:
-        if id_bulb == 0:
-            bulb0.turn_on()
-        elif id_bulb == 1:
-            bulb1.turn_on()
-    return ('', 204)
-# Turn the bulb off.
-@app.route('/stop_yeelight/<int:id_bulb>')
-def stop_yeelight(id_bulb=None):
-    if id_bulb != None:
-        if id_bulb == 0:
-            bulb0.turn_off()
-        elif id_bulb == 1:
-            bulb1.turn_off()
-    return ('', 204)
+
 
 # Toggle power.
 @app.route('/toggle_yeelight/<int:id_bulb>')
 def toggle_yeelight(id_bulb=None):
     if id_bulb != None:
         if id_bulb == 0:
-            bulb0.toggle()
+            bulb0.toggle(effect="sudden")
         elif id_bulb == 1:
-            bulb1.toggle()
+            bulb1.toggle(effect="sudden")
         elif id_bulb == 2:
-            bulb0.toggle()
-            bulb1.toggle()    
+            bulb0.toggle(effect="sudden")
+            bulb1.toggle(effect="sudden")    
     return ('', 204)
 
 
 ####################### TEST #############################
 
 
-flow = Flow(
-    count=0,  # Cycle forever.
-    transitions=transitions.christmas()
-)
+
 
 # Infinite color cycle
 @app.route('/start_color_cycle_yeelight/<int:id_bulb>')
-def cycle_yeelight(id_bulb=None):
+def start_flow_yeelight(id_bulb=None):
+    
+    flow = Flow(
+        count=0,  # Cycle forever.
+        transitions=transitions.christmas()
+    )
+
     if id_bulb != None:
         if id_bulb == 0:
             bulb0.start_flow(flow)
@@ -69,7 +57,7 @@ def cycle_yeelight(id_bulb=None):
 
 # Infinite color cycle stop
 @app.route('/stop_color_cycle_yeelight/<int:id_bulb>')
-def stop_cycle_yeelight(id_bulb=None):
+def stop_flow_yeelight(id_bulb=None):
     if id_bulb != None:
         if id_bulb == 0:
             bulb0.stop_flow()
@@ -78,6 +66,49 @@ def stop_cycle_yeelight(id_bulb=None):
         elif id_bulb == 2:
             bulb0.stop_flow()
             bulb1.stop_flow()    
+    return ('', 204)
+
+# Turn the bulb on.
+@app.route('/start_yeelight/<int:id_bulb>')
+def start_yeelight(id_bulb=None):
+    if id_bulb != None:
+        if id_bulb == 0:
+            bulb0.turn_on(effect="sudden")
+        elif id_bulb == 1:
+            bulb1.turn_on(effect="sudden")
+    return ('', 204)
+
+
+# Turn the bulb off.
+@app.route('/stop_yeelight/<int:id_bulb>')
+def stop_yeelight(id_bulb=None):
+    if id_bulb != None:
+        if id_bulb == 0:
+            bulb0.turn_off()
+        elif id_bulb == 1:
+            bulb1.turn_off()
+    return ('', 204)
+
+
+#
+@app.route('/start_color_cycle_yeelight/<int:id_bulb>/<rgb_value>')
+def rgb_yeelight(id_bulb=None, rgb_value="ffffff"):
+   
+    int_rgb = int(rgb_value, 16)
+
+    b_color = int_rgb & 0b11111111
+    g_color = int_rgb >> 8 & 0b11111111
+    r_color = int_rgb >> 16 & 0b11111111
+    
+    if id_bulb != None:
+        if id_bulb == 0:
+            bulb0.set_rgb(r_color, g_color, b_color, effect="sudden")
+        elif id_bulb == 1:
+            bulb1.set_rgb(r_color, g_color, b_color, effect="sudden")
+        elif id_bulb == 2:
+            bulb0.set_rgb(r_color, g_color, b_color, effect="sudden")
+            bulb1.set_rgb(r_color, g_color, b_color, effect="sudden")
+
     return ('', 204)
 
 
